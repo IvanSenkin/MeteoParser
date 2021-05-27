668e58ff130533718bddcc41bffea638b5b95ebc
IvanSenkin <83362469+IvanSenkin@users.noreply.github.com>
2021-05-27 14:10:21 +0300

    Добавьте файлы проекта.

---
+++ MeteoParser/DataProvaider.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Xml.Linq;
+using System.Linq;
+
+namespace MeteoParser
+{
+    class DataProvaider
+    {
+        static string[] urls;
+
+        static DataProvaider()
+        {
+            urls = new string[]
+            {@"https://xml.meteoservice.ru/export/gismeteo/point/32277.xml"};
+        }
+
+    HttpClient httpClient;
+    public DataProvaider()
+        {
+            httpClient = new HttpClient();
+        }
+        public ObservableCollection<WetherModel> GetWetherModels()
+        {
+            ObservableCollection<WetherModel> temp = new ObservableCollection<WetherModel>();
+
+            foreach (var url in urls)
+            {
+               var req =  httpClient.GetStringAsync(url).Result;
+                // Debug.WriteLine(req);
+                var ColWeather = XDocument.Parse(req)
+                      .Descendants("MMWEATHER")
+                      .Descendants("REPORT")
+                      .Descendants("TOWN")
+                      .Descendants("FORECAST")
+                      .ToList();
+            foreach (var FORECAST in ColWeather)
+            {
+                    temp.Add(
+                                new WetherModel()
+                                {
+                                    DATE = FORECAST.Attribute("day").Value,
+                                    TEMPERATURE = FORECAST.Element("TEMPERATURE").Attribute("max").Value,
+                                    PRESSURE = FORECAST.Element("PRESSURE").Attribute("max").Value,
+                                    WIND = FORECAST.Element("WIND").Attribute("max").Value,
+                                }
+                             );
+            }
+            }
+            return temp;
+        }
+    }
+}
---
+++ MeteoParser/WetherModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeteoParser
+{
+    class WetherModel
+    {
+        int t, p, W, d;
+        public string DATE { get { return $"{d.ToString()} число";} set { int.TryParse(value, out d); } }
+        public string TEMPERATURE { get { return $"{t.ToString()} градусов"; } set { int.TryParse(value, out t); } }// макс и мин
+        public string PRESSURE { get { return $"{p.ToString()} мм. рт. ст."; } set { int.TryParse(value, out p); } }
+        public string WIND { get { return $"{W.ToString()} м/с "; } set { int.TryParse(value, out W); } }
+
+        //public override string ToString()
+        //{
+        //    return $"{TEMPERATURE} градусов {PRESSURE} мм. рт. ст. {WIND} м.с. ";
+        //}
+    }
+}
